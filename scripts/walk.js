3f8610bf27b459fb038431b328658cad5c268de1
CUR53 <114309130+CUR-53@users.noreply.github.com>
2023-04-20 10:25:34 +0200

    update signs to bg img

--- scripts/walk.js
+++ scripts/walk.js
@@ -1,5 +1,5 @@
-let canvas = document.querySelector("#canvas");
-let ctx = canvas.getContext("2d");
+let canvas = document.querySelector('#canvas');
+let ctx = canvas.getContext('2d');
 let spriteSheet = new Image();
 let frameWidth = 100; // Bredde af hvert billede i spritesheetet
 let frameHeight = 172; // Højde af hvert billede i spritesheetet
@@ -7,30 +7,20 @@ let frameCount = 8; // Antal billeder i spritesheetet
 let currentFrame = 0; // Aktuelt rammeindeks
 let scrollTop = 0; // Scrolltop-værdi
 
-spriteSheet.src = "assets/img/walkGuy.png"; // Sti til dit spritesheet-billede
+spriteSheet.src = 'assets/img/knight.png'; // Sti til dit spritesheet-billede
 
-window.addEventListener("scroll", animateSprite);
-window.addEventListener("load", animateSprite); // Kald animateSprite manuelt når siden er indlæst
+window.addEventListener('scroll', animateSprite);
+window.addEventListener('load', animateSprite); // Kald animateSprite manuelt når siden er indlæst
 
 function animateSprite() {
   scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   currentFrame = Math.floor(scrollTop / frameHeight) % frameCount; // Beregn det aktuelle rammeindeks
-
+  
   // Ryd canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   // Tegn den aktuelle ramme fra spritesheetet
-  ctx.drawImage(
-    spriteSheet,
-    0,
-    currentFrame * frameHeight,
-    frameWidth,
-    frameHeight,
-    0,
-    0,
-    frameWidth,
-    frameHeight
-  );
+  ctx.drawImage(spriteSheet, 0, currentFrame * frameHeight, frameWidth, frameHeight, 0, 0, frameWidth, frameHeight);
 
   // Gentag animationen
   requestAnimationFrame(animateSprite);
